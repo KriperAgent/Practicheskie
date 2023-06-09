ac79c43f0573d71957ed29fb0cad7e8f894e9653
KriperAgent <103452440+KriperAgent@users.noreply.github.com>
2023-06-09 08:15:11 +0300

    Add files via upload

---
+++ pr10/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace PR10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.l3 = new System.Windows.Forms.Label();
+            this.l2 = new System.Windows.Forms.Label();
+            this.l1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(775, 389);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // l3
+            // 
+            this.l3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.l3.Location = new System.Drawing.Point(585, 405);
+            this.l3.Name = "l3";
+            this.l3.Size = new System.Drawing.Size(75, 36);
+            this.l3.TabIndex = 6;
+            this.l3.Text = "Информация";
+            this.l3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // l2
+            // 
+            this.l2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.l2.Location = new System.Drawing.Point(348, 405);
+            this.l2.Name = "l2";
+            this.l2.Size = new System.Drawing.Size(75, 36);
+            this.l2.TabIndex = 7;
+            this.l2.Text = "Форма";
+            this.l2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // l1
+            // 
+            this.l1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.l1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.l1.Location = new System.Drawing.Point(102, 405);
+            this.l1.Name = "l1";
+            this.l1.Size = new System.Drawing.Size(75, 36);
+            this.l1.TabIndex = 8;
+            this.l1.Text = "Вид";
+            this.l1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.l1);
+            this.Controls.Add(this.l2);
+            this.Controls.Add(this.l3);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label l3;
+        private System.Windows.Forms.Label l2;
+        private System.Windows.Forms.Label l1;
+    }
+}
+
---
+++ pr10/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR10
+{
+    public partial class Form1 : Form
+    {
+        
+        bool rectClicked = false;
+        bool circClicked = false;
+        bool squaClicked = false;
+
+        
+        Point rectLocation = new Point(10, 10);
+        Point circLocation = new Point(220, 10);
+        Point squaLocation = new Point(380, 10);
+
+        
+        int lastClicked = 0;
+
+        
+        int offsetX = 0;
+        int offsetY = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            
+            e.Graphics.FillRectangle(Brushes.Yellow, rectLocation.X, rectLocation.Y, 200, 100);
+            e.Graphics.FillEllipse(Brushes.Red, circLocation.X, circLocation.Y, 150, 150);
+            e.Graphics.FillRectangle(Brushes.Blue, squaLocation.X, squaLocation.Y, 150, 150);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            if (rectLocation.X <= e.X && e.X <= rectLocation.X + 200 &&
+                rectLocation.Y <= e.Y && e.Y <= rectLocation.Y + 100)
+            {
+                rectClicked = true;
+                lastClicked = 1;
+                offsetX = e.X - rectLocation.X;
+                offsetY = e.Y - rectLocation.Y;
+            }
+            else if (circLocation.X <= e.X && e.X <= circLocation.X + 150 &&
+                     circLocation.Y <= e.Y && e.Y <= circLocation.Y + 150)
+            {
+                circClicked = true;
+                lastClicked = 2;
+                offsetX = e.X - circLocation.X;
+                offsetY = e.Y - circLocation.Y;
+            }
+            else if (squaLocation.X <= e.X && e.X <= squaLocation.X + 150 &&
+                     squaLocation.Y <= e.Y && e.Y <= squaLocation.Y + 150)
+            {
+                squaClicked = true;
+                lastClicked = 3;
+                offsetX = e.X - squaLocation.X;
+                offsetY = e.Y - squaLocation.Y;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+            switch (lastClicked)
+            {
+                case 1:
+                    if (rectClicked)
+                    {
+                        rectLocation.X = e.X - offsetX;
+                        rectLocation.Y = e.Y - offsetY;
+                    }
+                    break;
+                case 2:
+                    if (circClicked)
+                    {
+                        circLocation.X = e.X - offsetX;
+                        circLocation.Y = e.Y - offsetY;
+                    }
+                    break;
+                case 3:
+                    if (squaClicked)
+                    {
+                        squaLocation.X = e.X - offsetX;
+                        squaLocation.Y = e.Y - offsetY;
+                    }
+                    break;
+            }
+
+            
+            if (lastClicked == 1 && new Rectangle(rectLocation.X, rectLocation.Y, 200, 100).Contains(e.X, e.Y))
+            {
+                BringToFront(pictureBox1, Brushes.Yellow, rectLocation);
+                l1.Text = "прямоугольник";
+            }
+            else if (lastClicked == 2 && new Rectangle(circLocation.X, circLocation.Y, 150, 150).Contains(e.X, e.Y))
+            {
+                BringToFront(pictureBox1, Brushes.Red, circLocation);
+                l1.Text = "круг";
+            }
+            else if (lastClicked == 3 && new Rectangle(squaLocation.X, squaLocation.Y, 150, 150).Contains(e.X, e.Y))
+            {
+                BringToFront(pictureBox1, Brushes.Blue, squaLocation);
+                l1.Text = "синий квадрат";
+            }
+
+            
+            pictureBox1.Invalidate();
+
+            
+            rectClicked = false;
+            circClicked = false;
+            squaClicked = false;
+            lastClicked = 0;
+        }
+
+        
+        private void BringToFront(PictureBox pictureBox, Brush color, Point location)
+        {
+            
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.FillRectangle(Brushes.White, 0, 0, bmp.Width, bmp.Height);
+            g.FillEllipse(Brushes.Red, circLocation.X, circLocation.Y, 150, 150);
+            g.FillRectangle(Brushes.Blue, squaLocation.X, squaLocation.Y, 150, 150);
+            g.FillRectangle(Brushes.Yellow, rectLocation.X, rectLocation.Y, 200, 100);
+
+            
+            pictureBox.Image = bmp;
+        }
+    }
+}
